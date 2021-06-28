ef6adb20aecc18f42b87ba5a593a6c41ecc40654
maurobdata <mb.ing.data@gmail.com>
2021-06-28 22:20:49 +0200

    Add models
    Generated and applied corresponding migrations

--- django_app/app/app/settings.py
+++ django_app/app/app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'url_analysers.apps.UrlAnalysersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_app/app/url_analysers/models.py
+++ django_app/app/url_analysers/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class UrlUnderInvestigation(models.Model):
+    mnemonic_name = models.CharField(max_length=200, null=True)
+    url = models.CharField(max_length=200)
+    regular_expression = models.CharField(max_length=200, null=True)
+    note = models.CharField(max_length=200, null=True)
+    requests_successfully = models.IntegerField(default=0)
+    requests_failed = models.IntegerField(default=0)
+    requests_warning = models.IntegerField(default=0)
+    favourite = models.BooleanField(default=False)
+
+
+class Investigation(models.Model):
+    url_under_investigation = models.ForeignKey(UrlUnderInvestigation, on_delete=models.CASCADE)
+    # response_time = request_end_time - request_start_time
+    request_start_time = models.DateField()
+    request_end_time = models.DateField(null=True)
+    head = models.CharField(max_length=200, null=True)
+    body = models.CharField(max_length=800, null=True)
+    info = models.CharField(max_length=200, null=True)
+    status_code = models.IntegerField(null=True)
