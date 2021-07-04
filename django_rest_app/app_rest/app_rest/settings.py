c03e1b1ede99f3ace9f6c4d982d54fc498ca720b
maurobdata <mb.ing.data@gmail.com>
2021-07-05 00:10:11 +0200

    Remove unused user and group

--- django_rest_app/app_rest/app_rest/settings.py
+++ django_rest_app/app_rest/app_rest/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'web_site_availability.apps.WebSiteAvailabilityConfig',
-    'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- django_rest_app/app_rest/app_rest/urls.py
+++ django_rest_app/app_rest/app_rest/urls.py
@@ -16,24 +16,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from user import views as user_views
-from web_site_availability import views as web_site_views
+from web_site_availability import views
 
 router = routers.DefaultRouter()
-router.register(r'check_requests', web_site_views.SingleCheckRequestViewSet, basename='check_request')
-router.register(r'sites', web_site_views.WebSiteViewSet, basename='sites')
-# router.register(r'site_check_requests/(?P<web_site_id>\d+)', web_site_views.WebSiteCheckRequestViewSet,
-#                 basename='site_check_requests')
-router.register(r'users', user_views.UserViewSet)
-router.register(r'groups', user_views.GroupViewSet)
+router.register(r'check_requests', views.SingleCheckRequestViewSet, basename='check_request')
+router.register(r'sites', views.WebSiteViewSet, basename='sites')
 
-# Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    # path('sites/', views.web_site, name='sites'),
-    path('sites/<int:web_site_id>/requests/', web_site_views.web_site_check_request, name='sites_requests'),
-    # path('check_requests/', views.single_check_request, name='check_requests'),
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    # sites check request
+    path('sites/<int:web_site_id>/requests/', views.web_site_check_request, name='sites_requests')
 ]
