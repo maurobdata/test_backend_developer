3999643d5b489fc07e967b771cdb0b5b12d9f9a9
maurobdata <mb.ing.data@gmail.com>
2021-07-04 22:18:22 +0200

    Add validation and sync web site
    Check url and regex. Sync n_requests success, warning and fail.

--- django_rest_app/app_rest/web_site_availability/views.py
+++ django_rest_app/app_rest/web_site_availability/views.py
@@ -1,10 +1,12 @@
+from django.core.exceptions import ValidationError
+from django.core.validators import URLValidator, RegexValidator
 from django.shortcuts import get_object_or_404
+
 from rest_framework import status
-
 from rest_framework.decorators import api_view
-
 from rest_framework.response import Response
 
+from .lib.custom_status_code import map_code_to_message
 from .lib.request_handler import get_generic_check_request
 from .models import WebSite, SingleCheckRequest
 
@@ -19,6 +21,12 @@ def web_site(request):
         serializer = WebSiteSerializer(web_sites, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
+        url = request.data.get('url')
+        try:
+            validator = URLValidator()
+            validator(url)
+        except ValidationError as url_error:
+            return Response(url_error, status=status.HTTP_400_BAD_REQUEST)
         serializer = WebSiteSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -35,11 +43,27 @@ def web_site_check_request(request, web_site_id):
         serializer = WebSiteCheckRequestSerializer(check_requests, many=True)
         return Response(serializer.data)
     elif request.method == 'POST':
+        regular_expression = request.data.get('regular_expression')
+        if regular_expression:
+            try:
+                validator = RegexValidator()
+                validator(regular_expression)
+            except ValidationError as reg_error:
+                return Response(reg_error, status=status.HTTP_400_BAD_REQUEST)
+
         request.data['url'] = web_site.url
         data_check_request = get_generic_check_request(request.data)
         check_request = web_site.websitecheckrequest_set.create(**data_check_request)
         serializer = WebSiteCheckRequestSerializer(instance=check_request, data=data_check_request)
         if serializer.is_valid():
+            code_context = map_code_to_message(data_check_request.get('status_code'))
+            if code_context['res'] == 'Success':
+                web_site.n_requests_success += 1
+            elif code_context['res'] == 'Warning':
+                web_site.n_requests_warning += 1
+            elif code_context['res'] == 'Fail':
+                web_site.n_requests_fail += 1
+            web_site.save()
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -54,6 +78,19 @@ def single_check_request(request):
         return Response(serializer.data)
     elif request.method == 'POST':
         url = request.data.get('url')
+        try:
+            validator = URLValidator()
+            validator(url)
+        except ValidationError as url_error:
+            return Response(url_error, status=status.HTTP_400_BAD_REQUEST)
+        regular_expression = request.data.get('regular_expression')
+        if regular_expression:
+            try:
+                validator = RegexValidator()
+                validator(regular_expression)
+            except ValidationError as reg_error:
+                return Response(reg_error, status=status.HTTP_400_BAD_REQUEST)
+
         data_check_request = get_generic_check_request(request.data)
         data_check_request['url'] = url
         check_request = SingleCheckRequest(**data_check_request)
