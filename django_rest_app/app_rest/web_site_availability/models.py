35db3df38d7f6f726dd6298a991c07cc5280c72a
maurobdata <mb.ing.data@gmail.com>
2021-07-05 03:10:54 +0200

    Remove unused completed_at

--- django_rest_app/app_rest/web_site_availability/models.py
+++ django_rest_app/app_rest/web_site_availability/models.py
@@ -5,7 +5,6 @@ from django.db import models
 class TimeStampMixin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     updated_at = models.DateTimeField(auto_now=True, editable=False)
-    completed_at = models.DateTimeField(null=True, blank=True, editable=False)
 
     class Meta:
         abstract = True
