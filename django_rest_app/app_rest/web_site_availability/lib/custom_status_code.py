cd726974a14304ace7b7093effbbb2d785755340
maurobdata <mb.ing.data@gmail.com>
2021-09-11 18:48:57 +0200

    Add pylint and pep8
    resolve some warnings

--- django_rest_app/app_rest/web_site_availability/lib/custom_status_code.py
+++ django_rest_app/app_rest/web_site_availability/lib/custom_status_code.py
@@ -1,8 +1,9 @@
+"""Customize http status code"""
 SUCCESS = "Success"
 FAIL = "Fail"
 WARNING = "Warning"
 ERROR = "Error"
-more_info = (
+MORE_INFO = (
     "\n More info about HTTP Status Codes at: "
     "https://www.restapitutorial.com/httpstatuscodes.html \n"
     " RFC: https://datatracker.ietf.org/doc/html/rfc1945#section-6.1.1"
@@ -11,42 +12,42 @@ more_info = (
 
 def _err():
     message_error = "Invalid http code"
-    message_error += more_info
+    message_error += MORE_INFO
     error = {"message": message_error, "res": ERROR}
     return error
 
 
 def _1xx():
     message = "This lets us know that the request was received"
-    message += more_info
+    message += MORE_INFO
     context = {"message": message, "res": WARNING}
     return context
 
 
 def _2xx():
     message = "This shows that the request was successful"
-    message += more_info
+    message += MORE_INFO
     context = {"message": message, "res": SUCCESS}
     return context
 
 
 def _3xx():
     message = "This is for redirects (temporary and permanent)"
-    message += more_info
+    message += MORE_INFO
     context = {"message": message, "res": WARNING}
     return context
 
 
 def _4xx():
     message = "Client errors"
-    message += more_info
+    message += MORE_INFO
     context = {"message": message, "res": FAIL}
     return context
 
 
 def _5xx():
     message = "Server errors"
-    message += more_info
+    message += MORE_INFO
     context = {"message": message, "res": FAIL}
     return context
 
@@ -64,12 +65,12 @@ def map_code_to_message(status_code):
     """Mapper to convert status code to understandable message"""
     # RFC https://datatracker.ietf.org/doc/html/rfc1945#section-6.1.1
     # specify 3 digits letters
-    if not len(str(status_code)) == 3:
+    if len(str(status_code)) != 3:
         # Execute the error
         return _err()
     # First digit of integer code
     family = str(status_code)[0]
     # Get the function from switcher dictionary
-    func = switcher.get(family, lambda: _err())
+    func = switcher.get(family)
     # Execute the function
     return func()
--- django_rest_app/app_rest/web_site_availability/lib/request_handler.py
+++ django_rest_app/app_rest/web_site_availability/lib/request_handler.py
@@ -1,3 +1,4 @@
+"""return requests analysis if is available or Python3 urllib instead"""
 import re
 import time
 
@@ -11,11 +12,20 @@ except ImportError:
 
 
 def get_generic_check_request(data):
+    """return dict {
+        lib_request_type: <class 'requests.models.Response'> or <class 'http.client.HTTPResponse'>,
+        response_time: elapsed time in seconds,
+        status_code: http status code,
+        response: http response,
+        regular_expression: regex used,
+        pattern_regular_expression: pattern matched,
+        match_regular_expression: True or False if match.
+    }"""
     result_data = {}
     # result_data['url'] = data.get('url')
     url = data.get("url")
     if not url:
-        return
+        return result_data
     try:  # Using requests
         request = requests.get(url)  # requests.models.Response
         result_data["lib_request_type"] = str((type(request)))
@@ -35,14 +45,16 @@ def get_generic_check_request(data):
             result_data["status_code"] = request.getcode()
             result_data["response"] = str(page)
         except AttributeError:  # Using Python3 urllib
-            urllib.urlopen(url)  # Should raise correct Exception
+            urllib.request.urlopen(
+                url
+            )  # Should raise correct Exception pylint: disable=R1732
 
     result_data["regular_expression"] = data.get("regular_expression")
     if result_data.get("regular_expression"):
         result_data["pattern_regular_expression"] = re.search(
             result_data.get("regular_expression"), result_data.get("response")
         )
-        result_data["match_regular_expression"] = (
-            True if result_data.get("pattern_regular_expression") else False
+        result_data["match_regular_expression"] = bool(
+            result_data.get("pattern_regular_expression")
         )
     return result_data
